8874b699fca6db01a5bcdc5f521fa5e63e6b7f48
Bilalzafar0020 <bilalafridi0020@gmail.com>
2023-05-10 16:28:59 +0500

    Drawing website

--- main.js
+++ main.js
@@ -1,121 +1,59 @@
-let canvas = document.querySelector("#Drawing-Board"); 
-
-//  canvas setting
-let cObject = canvas.getContext("2d");
-
-
-//  line-width
-lineWidth = document.querySelector("#line-Widther");
-
-
-// colors selections
-colors = document.querySelector("#stroke");
-
-
-//   Reset drawing board
-
-clear = document.querySelector("#Reset");
-
-
-//  download image
-
-save = document.querySelector("#download");
-
-
-
-
-// global variables
-let cDrawing= false;
+// Declare variables with const or let
+const canvas = document.querySelector("#Drawing-Board");
+const cObject = canvas.getContext("2d");
+const lineWidth = document.querySelector("#line-Widther");
+const colors = document.querySelector("#stroke");
+const clear = document.querySelector("#Reset");
+const save = document.querySelector("#download");
+let cDrawing = false;
 let cursorWidth = 3;
 
-
-/* we make variable above cDrawing = false and this function is for that work that when mouse
-is down  start making line for that we made addeventlistner below  and when up  stop it
- also for that we make a addeventlistner below  */ 
-let drawingStart = () =>{
-    cDrawing = true;
-    cObject.beginPath() /* their was a problem mouseup event that the new line start from 
-    the old position , it is a method it will solve it */ 
-
-  cObject.lineWidth = cursorWidth //start drawing according to the user selected cursor width   
-}
-
-
-
-
-// function for mouse move  
-let mouseDrawing = (e) =>{
-    if(!cDrawing) return;
-cObject.lineTo(e.offsetX , e.offsetY); // line according to mouse move event
-
-cObject.stroke() // filling lines 
-};
-
-
-
-// to return valuable width/height of element we have to set the canvas width/height + offset width/height
-window.addEventListener("load" , () =>{
- 
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+// Set canvas width and height on window load
+window.addEventListener("load", () => {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
 });
 
-
-
-
-
-
-
-//  Tools area
-
-// line width according to range value
-lineWidth.addEventListener("change", ()=> cursorWidth = lineWidth.value)
-
-
-//  colors stroke;
-//  change color  according to user selected stroke style
-colors.addEventListener("change", e => {
-if( e.target.id ==='stroke'){
-    cObject.strokeStyle = e.target.value;
+// Start drawing when the mouse is down
+const drawingStart = () => {
+  cDrawing = true;
+  cObject.beginPath();
+  cObject.lineWidth = cursorWidth;
 };
 
-})
+// Draw lines as the mouse moves
+const mouseDrawing = (e) => {
+  if (!cDrawing) return;
+  cObject.lineTo(e.offsetX, e.offsetY);
+  cObject.stroke();
+};
 
-//  Reset button
+// Update cursor width when the user changes the line width
+lineWidth.addEventListener("change", () => (cursorWidth = lineWidth.value));
 
-clear.addEventListener("click" , () =>{
+// Change stroke color when the user selects a color
+colors.addEventListener("change", (e) => {
+  if (e.target.id === "stroke") {
+    cObject.strokeStyle = e.target.value;
+  }
+});
 
-cObject.clearRect(0 , 0 , canvas.width , canvas.height) 
-}) ; 
+// Clear the canvas when the user clicks the reset button
+clear.addEventListener("click", () => {
+  cObject.clearRect(0, 0, canvas.width, canvas.height);
+});
 
+// Save the canvas as an image when the user clicks the download button
+save.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.download = `${Date.now()}.jpg`;
+  link.href = canvas.toDataURL();
+  link.click();
+});
 
-//  save image
-save.addEventListener("click", () =>{
- let link = document.createElement("a"); // create ancher tag (a) because it have href and values
-
- link.download = `${Date.now()}.jpg `; // put the today date in value of link + jpg format  we can also use png format by just putting png and in  toDateURL(image/png)
-
- 
- link.href = canvas.toDataURL(); //  put the canvas data in href ;
-
-
- link.click(); // click the link to download
-} )
-
-
-
-// when the mouse move make lines
-canvas.addEventListener("mousemove",mouseDrawing);
-
-
-//  when the mouse down then start drawing (update after mouse move)
-canvas.addEventListener("mousedown", drawingStart);
-
-
-
-//  when user up the mouse stop making lines
-canvas.addEventListener("mouseup", () => cDrawing = false);
-
-
-
-
+// Add event listeners to the canvas for drawing
+if (canvas) {
+  canvas.addEventListener("mousemove", mouseDrawing);
+  canvas.addEventListener("mousedown", drawingStart);
+  canvas.addEventListener("mouseup", () => (cDrawing = false));
+}
